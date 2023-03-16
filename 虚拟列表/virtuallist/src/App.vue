2d8612d4f76f7ce2e46841b2bef44f5004bd5fae
hlb_gdut <1563810807@qq.com>
2023-03-16 15:39:48 +0800

    暂存

--- 虚拟列表/virtuallist/src/App.vue
+++ 虚拟列表/virtuallist/src/App.vue
@@ -1,46 +1,46 @@
 <template>
   <div id="app">
-    <!-- <virtualList :listData="listData" :itemSize="itemSize" v-if="false"/> -->
-    <ElTableList></ElTableList>
+    <virtualList :listData="listData" :itemSize="itemSize" v-if="false"  />
+    <ElVList :listData="listData" :itemSize="itemSize"   />
+    <!-- <ElTableList></ElTableList> -->
   </div>
 </template>
 
 <script>
-// import virtualList from './components/virtualList.vue'
-import ElTableList from './components/ElTableList.vue'
+import virtualList from "./components/virtualList.vue";
+import ElVList from "./components/ElVList.vue";
+// import ElTableList from './components/ElTableList.vue'
 
-let listData = []
+let listData = [];
 for (let i = 0; i < 1000; i++) {
-  listData.push({ id: i, value: `第${i}条数据` })
+  listData.push({ id: i, value: `第${i}条数据` });
 }
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    // virtualList,
-    ElTableList
-    
+    virtualList,ElVList
+    // ElTableList
   },
   data() {
     return {
-      // listData: listData,
-      // itemSize: 100, //列表每项高度(px)
-    }
-  }
-}
+      listData: listData,
+      itemSize: 100, //列表每项高度(px)
+    };
+  },
+};
 </script>
 
 <style>
-
-html{
+html {
   height: 100%;
 }
-body{
+body {
   height: 100%;
-  margin:0;
+  margin: 0;
 }
-#app{
-  height:100%;
+#app {
+  height: 100%;
   text-align: center;
   margin-top: 60px;
 }
--- 虚拟列表/virtuallist/src/main.js
+++ 虚拟列表/virtuallist/src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import ElementUI from 'element-ui';
+import Vue from "vue";
+import App from "./App.vue";
+import ElementUI from "element-ui";
 import infiniteScroll from "vue-infinite-scroll";
-import 'element-ui/lib/theme-chalk/index.css';
-import 'default-passive-events'
+import "element-ui/lib/theme-chalk/index.css";
+import "default-passive-events";
+// import ElTableInfiniteScroll from 'el-table-infinite-scroll';
+
 Vue.use(ElementUI);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(infiniteScroll);
+// Vue.use(ElTableInfiniteScroll);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
